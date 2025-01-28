fa27fb0244ff8c04251e1f80389b2da45785ed97
Adam234123 <adampolgar5@gmail.com>
2025-01-28 21:28:44 +0100

    backend 2
    backend demo 2

--- dashboard.php
+++ dashboard.php
@@ -36,7 +36,7 @@ if (!isset($_SESSION['user_id'])) {
                         <a class="nav-link" href="about2.html">Rólunk</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="logout.php">Kijelentkezés</a>
+                        <a class="nav-link" href="http://localhost/barbershop/logout.php">Kijelentkezés</a>
                     </li>
                 </ul>
             </div>
